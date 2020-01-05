a352a958ece3408ab23244d85ce7c4089e3751f9
syarif hidayatullah <syarif.saungkertas@gmail.com>
2020-01-05 21:53:32 +0700

    save result to csv

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import mysql.connector
 from sqlalchemy import create_engine
 
 engine = create_engine('mysql+mysqlconnector://guest:relational@relational.fit.cvut.cz/UW_std')
-data = pd.read_sql_table('person', engine)
-print(data)
+person = pd.read_sql_table('person', engine)
+taught_by = pd.read_sql_table('taughtBy', engine)
+advised_by = pd.read_sql_table('advisedBy', engine)
+course = pd.read_sql_table('course', engine)
+
+person_join_taughtby = person.set_index('p_id').join(taught_by.set_index('p_id'))
+person_taughtby_join_advisedby = person_join_taughtby.join(advised_by.set_index('p_id'))
+person_taughtby_advisedby_reset_index = person_taughtby_join_advisedby.reset_index()
+person_taughtby_advisedby_join_course = person_taughtby_advisedby_reset_index.set_index('course_id').join(course.set_index('course_id'))
+full_dataset = person_taughtby_advisedby_join_course.reset_index()                                                                                                  
+full_dataset.to_csv('education.csv')
